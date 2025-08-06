fdcd146cb3a059aee2e53264208d3d24b92c898d
Горковец Роман <rrgorkovets@cloud.ru>
2025-08-06 22:33:20 +0300

    dev: lol

--- setup.py
+++ setup.py
@@ -3,8 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name="j2c",
     version="0.1",
+    description="JSON logs to CSV table converter",
     packages=find_packages(),
+    python_requires=">=3.6",
+    author="Roman Gorkovets",
+    author_email="saeretpk@gmail.com",
     entry_points={
-        "console_scripts": ["j2c = j2c"],
+        "console_scripts": [
+            "j2c=j2c:main",
+        ],
     },
 )
