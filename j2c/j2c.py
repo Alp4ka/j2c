eed45f3480dd32e594c8a54d6922569d674fcd26
Горковец Роман <rrgorkovets@cloud.ru>
2025-08-06 23:16:18 +0300

    flatten_dict

--- j2c/j2c.py
+++ j2c/j2c.py
@@ -2,6 +2,20 @@ import json
 import sys
 import csv
 from pathlib import Path
+from collections.abc import MutableMapping
+
+_SEP = '.'
+
+
+def flatten_dict(d: dict, parent_key: str = '') -> dict:
+    items = []
+    for k, v in d.items():
+        new_key = f"{parent_key}{_SEP}{k}" if parent_key else k
+        if isinstance(v, MutableMapping):
+            items.extend(flatten_dict(v, new_key).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
 
 
 def json_to_csv(input_file: Path, output_file: Path, delimiter_sym: str):
@@ -12,7 +26,7 @@ def json_to_csv(input_file: Path, output_file: Path, delimiter_sym: str):
     with open(input_file, 'r', encoding='utf-8') as f:
         for line_num, line in enumerate(f, 1):
             try:
-                data = json.loads(line.strip())
+                data = flatten_dict(json.loads(line.strip()))
                 for key in data.keys():
                     if key not in fieldnames:
                         fieldnames[key] = None
@@ -37,7 +51,7 @@ def json_to_csv(input_file: Path, output_file: Path, delimiter_sym: str):
         with open(input_file, 'r', encoding='utf-8') as f:
             for line_num, line in enumerate(f, 1):
                 try:
-                    data = json.loads(line.strip())
+                    data = flatten_dict(json.loads(line.strip()))
                     writer.writerow(data)
                 except json.JSONDecodeError as e:
                     print(
